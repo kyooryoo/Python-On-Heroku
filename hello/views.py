2bf93efdc42fa6b9805f9eaf753c97a212fa066e
Ling Jiang <kyooryoo@hotmail.com>
2017-10-27 18:11:02 -0400

    modified api sample data

--- hello/views.py
+++ hello/views.py
@@ -18,5 +18,5 @@ def db(request):
 
     return render(request, 'db.html', {'greetings': greetings})
 
-def 30DayChallenge(request):
+def sampledata(request):
     return render(request, '30DayChallenge')
